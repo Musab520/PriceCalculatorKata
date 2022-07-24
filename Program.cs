a75661cded134d76edd3870550a1fa1dd46b8b58
Musab Abuasi <abuasimusab@gmail.com>
2022-07-24 10:11:15 +0300

    Requirement 1 Tax

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+using PriceCalculatorKata;
+
+class Program
+{
+    public static void Main()
+    {
+        Product p1 = new Product("Gamestop", 12345, 20.25, 0.2);
+        Console.WriteLine(p1);
+    }
+}
